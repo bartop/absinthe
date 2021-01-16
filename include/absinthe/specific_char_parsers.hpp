d07bfa51d0a52ae685a70128fd16553bdf95cc79
Bartosz Miera (bartop) <bartosz.miera@gmail.com>
2021-01-16 19:55:20 +0100

    Added alpha and blank parsers to specific char parsers

--- include/absinthe/specific_char_parsers.hpp
+++ include/absinthe/specific_char_parsers.hpp
@@ -10,21 +10,48 @@
 namespace absinthe
 {
 
-class alnum
-{
+namespace detail {
+
+class function_char_parser {
 public:
-    constexpr alnum() = default;
+    using fun = int (*)(int);
+    function_char_parser(const std::string& name, fun function) :
+        m_name(name),  m_function(function) {}
 
     template<class It>
-    parse_result<It, char> parse(It begin, It end) const
+    constexpr parse_result<It, char> parse(It begin, It end) const
     {
-        if (begin == end || !std::isalnum(*begin))
-            return { begin, parser_error{ "alnum parser failed" } };
+        if (begin == end || !m_function(*begin))
+            return { begin, parser_error{ m_name + " parser failed" } };
 
         auto result = *begin;
 
         return { ++begin, result };
     }
+
+private:
+    std::string m_name;
+    fun m_function;
+};
+
+}
+
+class alnum : public detail::function_char_parser
+{
+public:
+    alnum() : function_char_parser{"alnum", &std::isalnum} {}
+};
+
+class alpha : public detail::function_char_parser
+{
+public:
+    alpha() : function_char_parser{"alpha", &std::isalpha} {}
+};
+
+class blank : public detail::function_char_parser
+{
+public:
+    blank() : function_char_parser{"blank", &std::isblank} {}
 };
 
 }
--- test/specific_char_parsers.cpp
+++ test/specific_char_parsers.cpp
@@ -12,30 +12,93 @@ TEMPLATE_TEST_CASE(
     "[parser][char]",
     std::string, std::vector<char>, std::list<char>)
 {
-	auto parser = absinthe::alnum{};
+    SECTION("alnum parser")
+    {
+        auto parser = absinthe::alnum{};
 
-	SECTION("parser accepts matching input string")
-	{
-		auto input_string = GENERATE(as<std::string>{}, "a", "R", "3");
-        auto input = TestType(input_string.begin(), input_string.end());
+        SECTION("parser accepts matching input string")
+        {
+            auto input_string = GENERATE(as<std::string>{}, "a", "R", "3");
+            auto input = TestType(input_string.begin(), input_string.end());
 
-		auto [it, parsed] = parse(input, parser);
+            auto [it, parsed] = parse(input, parser);
 
-		REQUIRE(it == ++input.begin());
-		REQUIRE(std::get<char>(parsed) == input_string.front());
-	}
-	
-	SECTION("parser does not accept not matching input string")
-	{
-        using absinthe::parser_error;
-		auto input_string = GENERATE(as<std::string>{}, "[", "", "&");
-        auto input = TestType(input_string.begin(), input_string.end());
+            REQUIRE(it == ++input.begin());
+            REQUIRE(std::get<char>(parsed) == input_string.front());
+        }
+        
+        SECTION("parser does not accept not matching input string")
+        {
+            using absinthe::parser_error;
+            auto input_string = GENERATE(as<std::string>{}, "[", "", "&");
+            auto input = TestType(input_string.begin(), input_string.end());
 
-		auto [it, parsed] = parse(input, parser);
+            auto [it, parsed] = parse(input, parser);
 
-		REQUIRE(it == input.begin());
+            REQUIRE(it == input.begin());
 
-		auto error = std::get_if<parser_error>(&parsed);
-		REQUIRE(error != nullptr);
-	}
+            auto error = std::get_if<parser_error>(&parsed);
+            REQUIRE(error != nullptr);
+        }
+    }
+
+    SECTION("blank parser")
+    {
+        auto parser = absinthe::blank{};
+
+        SECTION("parser accepts matching input string")
+        {
+            auto input_string = GENERATE(as<std::string>{}, " ", "\t");
+            auto input = TestType(input_string.begin(), input_string.end());
+
+            auto [it, parsed] = parse(input, parser);
+
+            REQUIRE(it == ++input.begin());
+            REQUIRE(std::get<char>(parsed) == input_string.front());
+        }
+        
+        SECTION("parser does not accept not matching input string")
+        {
+            using absinthe::parser_error;
+            auto input_string = GENERATE(as<std::string>{}, "", "\n", "1", "[", "a", "&");
+            auto input = TestType(input_string.begin(), input_string.end());
+
+            auto [it, parsed] = parse(input, parser);
+
+            REQUIRE(it == input.begin());
+
+            auto error = std::get_if<parser_error>(&parsed);
+            REQUIRE(error != nullptr);
+        }
+    }
+
+    SECTION("alpha parser")
+    {
+        auto parser = absinthe::alpha{};
+
+        SECTION("parser accepts matching input string")
+        {
+            auto input_string = GENERATE(as<std::string>{}, "a", "R", "g");
+            auto input = TestType(input_string.begin(), input_string.end());
+
+            auto [it, parsed] = parse(input, parser);
+
+            REQUIRE(it == ++input.begin());
+            REQUIRE(std::get<char>(parsed) == input_string.front());
+        }
+        
+        SECTION("parser does not accept not matching input string")
+        {
+            using absinthe::parser_error;
+            auto input_string = GENERATE(as<std::string>{}, "1", "[", "", "&");
+            auto input = TestType(input_string.begin(), input_string.end());
+
+            auto [it, parsed] = parse(input, parser);
+
+            REQUIRE(it == input.begin());
+
+            auto error = std::get_if<parser_error>(&parsed);
+            REQUIRE(error != nullptr);
+        }
+    }
 }
