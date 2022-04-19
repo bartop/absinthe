547b75e32ddc2666cd9fb67a7c097566ffb12234
Bartosz Miera <bartosz.miera@gmail.com>
2022-04-19 19:58:58 +0200

    Made building more generic

--- build.bash
+++ build.bash
@@ -2,8 +2,10 @@
 
 set -e
 
-conan install -if .module-path .
+PROCESSOR_COUNT=`nproc`
+
+conan install --install-folder .module-path .
 cmake -S . -B .build -DCMAKE_MODULE_PATH=.module-path
 
-make all -C .build -j8
-make test -C .build -j8
+cmake --build .build -j${PROCESSOR_COUNT}
+cmake --build .build -j${PROCESSOR_COUNT} --target test
--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 class Absinthe(ConanFile):
     generators = "cmake_find_package"
@@ -8,6 +8,13 @@ class Absinthe(ConanFile):
     version = "0.1.0"
     exports_sources = "include/*"
     no_copy_source = True
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+        cmake.install()
 
     def package(self):
         self.copy("*.hpp")
